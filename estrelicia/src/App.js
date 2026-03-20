6036ab626d8f109f56a0d2cfc8064c492a420b29
HenriqueCL <rique130@gmail.com>
2026-03-20 02:26:01 +0000

    Updated font
    Updated font

--- estrelicia/src/App.js
+++ estrelicia/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
 import Home from './components/Pages/Home';
-import Rent from './components/Pages/Rent';
 import Calender from './components/Pages/Calender';
 import SingUp from './components/Pages/SignUp';
 import Login from './components/Pages/Login';
